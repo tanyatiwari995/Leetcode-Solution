c081066c27422633d1976d806e89cdd13a1af904
Tanya Tiwari <getrjtanyatiwari@gmail.com>
2025-03-19 23:11:27 +0530

    Add files via upload

---
+++ day7/index.java
@@ -0,0 +1,26 @@
+package day7;
+
+public class index {
+    
+
+class Solution {
+    public int minOperations(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            if (nums[i] == 0) {
+                nums[i] ^= 1;
+                nums[i + 1] ^= 1;
+                nums[i + 2] ^= 1;
+                count++;
+            }
+        }
+
+        return (nums[n - 2] == 1 && nums[n - 1] == 1) ? count : -1;
+    }
+}
+}
+
+
+//3191. Minimum Operations to Make Binary Array Elements Equal to One I
